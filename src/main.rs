1aeb497564b7e2dc8d31de1c6aafa3983a4f8c6b
taskooh <taskooh@github.com>
2023-08-23 21:20:57 +0900

    :sparkles: Add wasm_logger

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 use dioxus::prelude::*;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     dioxus_web::launch(App);
 }
 
 fn App(cx: Scope) -> Element {
+    log::debug!("App");
     cx.render(rsx! {
         div {
             "hello world"
